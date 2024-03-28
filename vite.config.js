75ee8a0826d20d34ba88e9c93937639b6f1e7fc7
Hubery Yang <78254474+Hub-yang@users.noreply.github.com>
2024-03-28 22:30:58 +0800

    feat: init devtools next

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
+import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VueDevTools(),
     AutoImport({
       imports: ['vue', 'pinia', 'vue-router', '@vueuse/core'],
       dirs: ['./src/api/**'],
-      resolvers: [],
     }),
   ],
   server: {
